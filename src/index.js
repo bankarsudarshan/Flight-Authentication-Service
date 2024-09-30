c331d7c19d6f960fc4c09f2930af2e4cb4e434bd
coderbanda-22 <sudarshanbankar22@gmail.com>
2024-09-30 19:29:43 +0530

    Added User and Role models and associated them using many to many association via a User_Roles through table

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const apiRoutes = require('./routes');
 const UserService = require('./services/user-service');
 
 const express = require("express");
+const db = require('./models/index');
 const app = express(); // create an express app
 
 const prepareAndStartServer = () => {
@@ -14,8 +15,6 @@ const prepareAndStartServer = () => {
 
     app.listen(3001, () => {
         console.log(`server up and running on port ${PORT}`);
-        // const response = UserService.verifyToken('eyJhbGciOiJIUzINiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkb2xvdmVzc2RvQGdtYWlsLmNvbSIsImlkIjoxMCwiaWF0IjoxNzI3Njg5ODI4LCJleHAiOjE3Mjc2OTM0Mjh9.gAFjIkHCwFFus-cidCV-2CEdDzy1cTgAL4V8bcJew0Y');
-        // console.log(response);
     })
 }
 
